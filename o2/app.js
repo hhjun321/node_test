2ef7fbefc51cdc9f56f3d14ddcfb0a5dff0f7819
kimjunghyun <jjung3416@gmail.com>
2018-11-21 18:01:09 +0900

    webstorm commit

--- o2/app.js
+++ o2/app.js
@@ -1,34 +1,159 @@
 var express = require('express');
-var passport = require('passport');
 
-var GoogleStrategy = require( 'passport-google-oauth20' ).Strategy
+var routes = require('./routes');
+
+var user = require('./routes/user');
+
+var http = require('http');
+
+var path = require('path');
+
+
+
+var passport = require('passport')
+
+    , FacebookStrategy = require('passport-facebook').Strategy;
+
+
+
+// serialize
+
+// 인증후 사용자 정보를 세션에 저장
 
 passport.serializeUser(function(user, done) {
-	    done(null, user);
+
+    console.log('serialize');
+
+    done(null, user);
+
 });
 
-passport.deserializeUser(function(obj, done) {
-	    done(null, obj);
+
+
+
+
+// deserialize
+
+// 인증후, 사용자 정보를 세션에서 읽어서 request.user에 저장
+
+passport.deserializeUser(function(user, done) {
+
+    //findById(id, function (err, user) {
+
+    console.log('deserialize');
+
+    done(null, user);
+
+    //});
+
 });
 
-passport.use(new GoogleStrategy({
-	        clientID: '492957928974-m787u6t7r2lkp847sttu8gusi8bc0jja.apps.googleusercontent.com',
-	        clientSecret: 'P4Wqn5OOxwZ3jKfZ992VZwGG',
-	        callbackURL: 'www.naver.com'
-	    }, function(accessToken, refreshToken, profile, done) {
-		            process.nextTick(function() {
-				                user = profile;
-				                return done(null, user);
-				            });
-		        }
+
+
+passport.use(new FacebookStrategy({
+
+        clientID: '454174421775076',
+
+        clientSecret: 'd5ee2fc7cd19ce875ea390ec69556ea1',
+
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+
+    },
+
+    function(accessToken, refreshToken, profile, done) {
+
+        console.log(profile);
+
+        done(null,profile);
+
+    }
+
 ));
 
 var app = express();
-var AWS = require('aws-sdk');
-AWS.config.region = 'ap-northeast-2';
-var ec2 = new AWS.EC2();
-app.get('/', function(req, res){ res.send('hello world'); });
-app.get('/ec2', function(req, res){ ec2.describeInstances( {}, function(err, data){ res.json(data); }); });
-app.get('/auth/google',passport.authenticate('google', {scope:['profile']}));
-app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), function(req, res){ res.redirect('/'); });
-app.listen(80, function(){ console.log('connect 80 port'); });
+
+
+
+// all environments
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+//app.use(express.favicon());
+
+//app.use(express.logger('dev'));
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+app.use(express.methodOverride());
+
+app.use(express.session({ secret: 'your secret here' }));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(app.router);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// development only
+
+if ('development' == app.get('env')) {
+
+    app.use(express.errorHandler());
+
+}
+
+
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+
+    passport.authenticate('facebook', { successRedirect: '/login_success',
+
+        failureRedirect: '/login_fail' }));
+
+app.get('/login_success', ensureAuthenticated, function(req, res){
+
+    res.send(req.user);
+
+});
+
+app.get('/logout', function(req, res){
+
+    req.logout();
+
+    res.redirect('/');
+
+});
+
+function ensureAuthenticated(req, res, next) {
+
+    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
+
+    if (req.isAuthenticated()) { return next(); }
+
+    // 로그인이 안되어 있으면, login 페이지로 진행
+
+    res.redirect('/');
+
+}
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+
+    console.log('Express server listening on port ' + app.get('port'));
+
+});
+
+
